5c949a1943009a5e265835fdc0aaac8c6c99d2dd
PietroSantos280 <santospietro0910@gmail.com>
2026-08-20 12:12:53 -0300

    Aula 4 - 20/08/2026

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'conexao.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
@@ -10,7 +11,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
     $resultado = mysqli_query($conexao, $sql);
     if (mysqli_num_rows($resultado) == 1) {
-        $mensagem = "Login realizado com sucesso!";
+        $usuario = mysqli_fetch_assoc($resultado);
+        $_SESSION['usuario_id'] = $usuario['id'];
+        $_SESSION['usuario_nome'] = $usuario['nome'];
+        header("Location: produtos/listar.php");
+        exit;
     } else {
         $mensagem = "E-mail ou senha inválidos.";
     }
---
+++ produtos/listar.php
@@ -0,0 +1,11 @@
+<?php
+include '../conexao.php';
+?>
+<?php include 'verifica_login.php'; ?>
+<?php include '../cabecalho.php'; ?>
+<link rel="stylesheet" href="../css/style.css">
+<main>
+    <p>Bem-vindo(a), <?php echo $_SESSION['usuario_nome']; ?>!</p>
+    <!-- conteúdo da página -->
+</main>
+<?php include '../rodape.php'; ?>
