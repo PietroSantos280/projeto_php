4b46379b31fb92c40d1b2de18544ed3858362839
PietroSantos280 <santospietro0910@gmail.com>
2026-08-27 12:08:03 -0300

    aula 5 - 27/08/2026

--- cabecalho.php
+++ cabecalho.php
@@ -1,5 +1,5 @@
 <?php
-    $base_url = '/projeto_php/';
+$base_url = '/projeto_php/';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -8,16 +8,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $base_url; ?>css/style.css">
-    <title>Projeto PHP - CRUD</title>    
+    <title>Projeto PHP - CRUD</title>
 </head>
 
 <body>
     <header>
         <h1>Sistema de Produtos</h1>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle-input">
+
+        <!-- Ícone do Sanduíche com as 3 barrinhas -->
+        <label for="nav-toggle" class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
         <nav>
             <a href="<?php echo $base_url; ?>index.php">Início</a>
             <a href="<?php echo $base_url; ?>produtos/listar.php">Produtos</a>
             <a href="<?php echo $base_url; ?>login.php">Login</a>
-            <a href="<?php echo $base_url; ?>logout.php">sair</a>
+            <a href="<?php echo $base_url; ?>logout.php">Sair</a>
         </nav>
     </header>
